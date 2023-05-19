b7a23304cdd53c8c30da8fdf6912a392b3d95e36
LittleDevil <119108710+LittleDevil013@users.noreply.github.com>
2023-05-19 08:46:09 +0200

    add myPug

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let openText = document.getElementById('openText');
 let openScreen = document.getElementById('openScreen');
 let textArea = document.getElementById('textArea');
+let myPug;
+
 
 function hideOpenText() {
     openText.style.visibility = 'hidden';
@@ -25,15 +27,57 @@ window.addEventListener('keydown' , function(event) {
             let backgroundMusic = new Audio('../mopedAzUrben/music/backMusic.mp3');
             backgroundMusic.play();
             backgroundMusic.loop = true;
-            createGameArea();
+            gameArea.start();
+            myPug = new component(45, 30, "../mopedAzUrben/images/pug.png", 10, 50, "image");
+           
            
         }
 });
 
-function createGameArea() {
-    let canvas = document.createElement('canvas');
-    canvas.setAttribute("id" , "gameArea");
-    context = canvas.getContext('2d');
-    document.body.insertBefore(canvas, document.body.childNodes[0]);
+let gameArea = {
+    canvas : document.createElement('canvas'),
+    
+
+    start : function() {
+        this.canvas.setAttribute("id", "gameArea");
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+
+    clear : function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    if (type == 'image') {
+        this.image = new Image();
+        this.image.src = color;
+    }
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+
+    this.update = function () {
+        ctx = gameArea.context;
+        if (type == "image") {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
+        else {
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+
     
 }
+
+function updateGameArea() {
+    gameArea.clear();
+    myPug.update();
+}
+
+
